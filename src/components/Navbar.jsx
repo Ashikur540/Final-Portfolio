7be7d02ac3d82a820184de1a73bc149d9b0b4fcd
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2023-01-18 01:31:29 +0600

    projects going navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 import { HiOutlineBars3BottomRight, HiXMark } from "react-icons/hi2";
 const Navbar = ({ setSelectedPage, selectedPage }) => {
     const [isClicked, setIsclicked] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+
+        return () => window.onscroll = null;
+    }
+
     const LINKS = [
         { name: "Home_", ref: "#HERO" }, { name: "About_", ref: "#ABOUT" }, { name: "Skills_", ref: "#SKILLS" }, { name: "Projects_", ref: "#PROJECTS" }, { name: "contact_", ref: "#CONTACT" },
     ]
     return (
-        <nav className={`bg-transparent z-[999]  py-1 w-full fixed top-0 right-0 left-0 flex items-center justify-between pl-10 md:px-14 `}>
+        <nav className={`${isScrolled ? 'bg-white/20 backdrop-blur-md duration-500 ease-linear' : 'bg-transparent'} z-[999]  py-1 w-full fixed top-0 right-0 left-0 flex items-center justify-between pl-10 md:px-14 `}>
             <h4 className="text-3xl">AR</h4>
 
             <div className='md:hidden fixed right-5 cursor-pointer z-20 top-2 text-3xl font-bold transform transition duration-300 ease-linear'>
